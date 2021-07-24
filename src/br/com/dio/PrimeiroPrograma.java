bd26fa8ee3dc3e16d43c70e02995ae02294a8d9e
alyson-rdev <83791574+alyson-rdev@users.noreply.github.com>
2021-07-24 17:42:35 -0300

    Segundo commit realizado através do versionamento integrado da IDE intelliJ

--- src/br/com/dio/PrimeiroPrograma.java
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -8,8 +8,47 @@ public class PrimeiroPrograma {
         gato gato = new gato();
         System.out.println(gato);
 
+        String nome;
+        int numPags;
+        Livro livro = new Livro(nome = "O problema dos 3 corpos", numPags = 300);
+        System.out.println(livro);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a+b));*/
+    }
+}
+
+class Livro{
+    private String nome;
+    private Integer numPags;
+
+    public Livro(String nome, Integer numPags) {
+        this.nome = nome;
+        this.numPags = numPags;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPags() {
+        return numPags;
+    }
+
+    public void setNumPags(Integer numPags) {
+        this.numPags = numPags;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPags=" + numPags +
+                '}';
     }
 }
